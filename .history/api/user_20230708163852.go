3cd2fa6f8d16fee5bedb7020b7228b0bd44f4ed7
heypanda8 <ruth.empremiado@gmail.com>
2023-07-08 17:47:00 +0800

    -

---
+++ .history/api/user_20230708163852.go
@@ -0,0 +1,118 @@
+package api
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/dafalo/CMMS/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+	db := GormDB()
+
+	product := models.User{}
+	name := r.FormValue("name")
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	department := r.FormValue("department")
+
+	stat := r.FormValue("position")
+
+	product.Name = name
+	product.Username = username
+	product.Password = hashPassword(password)
+	product.Type = stat
+
+	db.Save(&product)
+
+	sqlDB, _ := db.DB()
+	sqlDB.Close()
+
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+
+	db := GormDB()
+
+	item := []models.User{}
+	db.Find(&item)
+
+	data := map[string]interface{}{
+		"status": "ok",
+		"item":   item,
+	}
+	ReturnJSON(w, r, data)
+	sqlDB, _ := db.DB()
+	sqlDB.Close()
+}
+
+func ChangePassword(w http.ResponseWriter, r *http.Request) {
+
+	db := GormDB()
+	product := models.User{}
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	db.Where("username", username).Find(&product)
+	product.Password = hashPassword(password)
+	product.Default = "1"
+	db.Save(&product)
+
+}
+
+func OTP(w http.ResponseWriter, r *http.Request) {
+
+	db := GormDB()
+	product := models.User{}
+	user, _ := r.Cookie("id")
+
+	db.Where("id", user.Value).Find(&product)
+
+	db.Save(&product)
+
+}
+
+func EditUser(w http.ResponseWriter, r *http.Request) {
+
+	db := GormDB()
+	id, _ := strconv.Atoi(r.FormValue("id"))
+	product := models.User{}
+	name := r.FormValue("name")
+	stat := r.FormValue("type")
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	action := r.FormValue("action")
+
+	db.Where("id", id).Find(&product)
+
+	if action == "edit" {
+		product.Name = name
+		product.Username = username
+		product.Password = hashPassword(password)
+	} else {
+		product.Name = name
+		product.Type = stat
+	}
+
+	db.Save(&product)
+
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+	db := GormDB()
+	id, _ := strconv.Atoi(r.FormValue("id"))
+	item := models.User{}
+	db.Where("id", id).Statement.Delete(&item)
+
+	sqlDB, _ := db.DB()
+	sqlDB.Close()
+
+}
+
+func hashPassword(pass string) string {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(pass), 14)
+	return string(bytes)
+}
---
+++ .history/models/user_20230708152916.go
@@ -0,0 +1,22 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	ID         uint `gorm:"primaryKey"`
+	Username   string
+	Password   string
+	Name       string
+	Type       string
+	Department string
+	Position   string
+	Date       string
+	Number     string
+	Default    string
+	Picture    string
+	Deleted    gorm.DeletedAt
+}
+
+func (u *User) String() string {
+	return u.Name
+}
