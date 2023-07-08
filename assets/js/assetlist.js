00d72ef6697f4f538fbe874924cdeaed73baf2a6
john carlos orijuela <johncarlosorijuela@gmail.com>
2023-07-08 17:54:12 +0800

    new chnages

---
+++ assets/js/assetlist.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $('#example').DataTable();
+});
+
+
+document.querySelector(".btn1").addEventListener("click",function(){
+    document.querySelector(".form").classList.add("active")
+})
