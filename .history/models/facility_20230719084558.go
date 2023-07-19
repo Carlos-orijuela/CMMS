904d982d18dd90f2af6ad9691b17f1650387201a
heypanda8 <ruth.empremiado@gmail.com>
2023-07-19 12:24:52 +0800

    --

---
+++ .history/models/facility_20230719084558.go
@@ -0,0 +1,38 @@
+package models
+
+import "gorm.io/gorm"
+
+type Facility struct {
+	ID             uint `gorm:"primaryKey"`
+	Name           string
+	Description    string
+	Image          string
+	ParentFacility string
+	Code           string // ?AUTO GENERATE
+	Category       string // NOTE: DDL >> Storage, Plants, Building
+	FilesORManuals string
+	RunningHouse   string // ?YES OR NO
+	RunningHr      string
+	QRCode         string // ?AUTO GENERATE
+	GoogleMaps     string
+	Address        string
+	City           string
+	Country        string
+	Deleted        gorm.DeletedAt
+}
+
+type ChildFacility struct {
+	ID         uint `gorm:"primaryKey"`
+	Name       string
+	Facility   Facility
+	FacilityID string
+	Code       string
+	QRCode     string
+	Deleted    gorm.DeletedAt
+}
+
+// ?FACILITY RELATIONSHIP
+// *LOCATION TABLE
+// *EQUIPMENT TABLE
+// *STOOLS TABLE
+// *PARTS TABLE
